1d57cc9545a10ba881f3c9bdb8dee30a95466975
choe-yujin <dev.yujinchoe@gmail.com>
2025-01-30 12:25:46 +0900

    style(typography): Define custom typography styles using Pretendard font

--- app/src/main/java/com/timor/kidsstory/domain/model/StoryPage.kt
+++
@@ -1,7 +0,0 @@
-package com.timor.kidsstory.domain.model
-
-data class StoryPage(
-    val pageNumber: Int,
-    val imageFileName: String,
-    val storyTexts: List<String>  // span id별 텍스트 목록
-)
--- app/src/main/java/com/timor/kidsstory/ui/theme/Type.kt
+++ app/src/main/java/com/timor/kidsstory/ui/theme/Type.kt
@@ -2,33 +2,136 @@ package com.timor.kidsstory.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.timor.kidsstory.R
 
 // Set of Material typography styles to start with
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+    displayLarge = AppTextStyles.titleTextBold,
+    displayMedium = AppTextStyles.titleTextRegular,
+    displaySmall = AppTextStyles.headerTextBold,
+    headlineLarge = AppTextStyles.headerTextRegular,
+    headlineMedium = AppTextStyles.largeTextBold,
+    headlineSmall = AppTextStyles.largeTextRegular,
+    titleLarge = AppTextStyles.mediumTextBold,
+    titleMedium = AppTextStyles.mediumTextRegular,
+    bodyLarge = AppTextStyles.normalTextBold,
+    bodyMedium = AppTextStyles.normalTextRegular,
+    labelLarge = AppTextStyles.smallTextBold,
+    labelMedium = AppTextStyles.smallTextRegular,
+    labelSmall = AppTextStyles.smallerTextBold
+)
+
+val pretendardFamily = FontFamily(
+    Font(R.font.pretendard_variable) // Variable Font 사용
+)
+
+object AppTextStyles {
+    // Title Text Styles
+    val titleTextBold = TextStyle(
+        fontSize = 50.sp,
+        lineHeight = 75.sp,
+        fontWeight = FontWeight.Bold,
+        fontFamily = pretendardFamily
+    )
+
+    val titleTextRegular = TextStyle(
+        fontSize = 50.sp,
+        lineHeight = 75.sp,
         fontWeight = FontWeight.Normal,
+        fontFamily = pretendardFamily
+    )
+
+    // Header Text Styles
+    val headerTextBold = TextStyle(
+        fontSize = 30.sp,
+        lineHeight = 45.sp,
+        fontWeight = FontWeight.Bold,
+        fontFamily = pretendardFamily
+    )
+
+    val headerTextRegular = TextStyle(
+        fontSize = 30.sp,
+        lineHeight = 45.sp,
+        fontWeight = FontWeight.Normal,
+        fontFamily = pretendardFamily
+    )
+
+    // Large Text Styles
+    val largeTextBold = TextStyle(
+        fontSize = 20.sp,
+        lineHeight = 30.sp,
+        fontWeight = FontWeight.Bold,
+        fontFamily = pretendardFamily
+    )
+
+    val largeTextRegular = TextStyle(
+        fontSize = 20.sp,
+        lineHeight = 30.sp,
+        fontWeight = FontWeight.Normal,
+        fontFamily = pretendardFamily
+    )
+
+    // Medium Text Styles
+    val mediumTextBold = TextStyle(
+        fontSize = 18.sp,
+        lineHeight = 27.sp,
+        fontWeight = FontWeight.Bold,
+        fontFamily = pretendardFamily
+    )
+
+    val mediumTextRegular = TextStyle(
+        fontSize = 18.sp,
+        lineHeight = 27.sp,
+        fontWeight = FontWeight.Normal,
+        fontFamily = pretendardFamily
+    )
+
+    // Normal Text Styles
+    val normalTextBold = TextStyle(
         fontSize = 16.sp,
         lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
+        fontWeight = FontWeight.Bold,
+        fontFamily = pretendardFamily
     )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+
+    val normalTextRegular = TextStyle(
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
         fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+        fontFamily = pretendardFamily
     )
-    */
-)
+
+    // Small Text Styles
+    val smallTextBold = TextStyle(
+        fontSize = 14.sp,
+        lineHeight = 21.sp,
+        fontWeight = FontWeight.Bold,
+        fontFamily = pretendardFamily
+    )
+
+    val smallTextRegular = TextStyle(
+        fontSize = 14.sp,
+        lineHeight = 21.sp,
+        fontWeight = FontWeight.Normal,
+        fontFamily = pretendardFamily
+    )
+
+    // Smaller Text Styles
+    val smallerTextBold = TextStyle(
+        fontSize = 11.sp,
+        lineHeight = 17.sp,
+        fontWeight = FontWeight.Bold,
+        fontFamily = pretendardFamily
+    )
+
+    val smallerTextRegular = TextStyle(
+        fontSize = 11.sp,
+        lineHeight = 17.sp,
+        fontWeight = FontWeight.Normal,
+        fontFamily = pretendardFamily
+    )
+}
