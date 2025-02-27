10362041126de3630c015b7eb8b35ebf48bfb34e
JaeYoun Kim <rlawodusx@naver.com>
2025-02-28 01:02:18 +0900

    feat: Add Horizontal Pager Filp Animation

---
+++ app/src/main/java/com/timor/kidsstory/presentation/reader/components/pagetest/PageFlapType.kt
@@ -0,0 +1,14 @@
+package com.timor.kidsstory.presentation.reader.components.pagetest
+
+import androidx.compose.ui.graphics.Shape
+
+
+/*
+* 비트맵 회전 담당
+* */
+internal sealed class PageFlapType(val shape: Shape) {
+    data object Top : PageFlapType(TopShape)
+    data object Bottom : PageFlapType(BottomShape)
+    data object Left : PageFlapType(LeftShape)
+    data object Right : PageFlapType(RightShape)
+}
