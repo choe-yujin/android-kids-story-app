19f8a79b68e4b3865d474c75b2b27b5508eded40
JaeYoun Kim <rlawodusx@naver.com>
2025-03-01 11:41:47 +0900

    feat: Add SpeechRecognizer Object

---
+++ app/src/main/java/com/timor/kidsstory/domain/util/SpeechRecognizerHelper.kt
@@ -0,0 +1,59 @@
+package com.timor.kidsstory.domain.util
+
+import android.content.Intent
+import android.os.Bundle
+import android.speech.RecognitionListener
+import android.speech.SpeechRecognizer
+import javax.inject.Inject
+import javax.inject.Singleton
+
+
+// 음성 인식 객체
+@Singleton
+class SpeechRecognizerHelper @Inject constructor(
+    private val speechRecognizer: SpeechRecognizer,
+    private val recognizerIntent: Intent
+) {
+    private var onResultCallback: ((String) -> Unit)? = null
+
+    // 음성 인식 시작
+    fun startListening(onResult: (String) -> Unit) {
+        onResultCallback = onResult
+        speechRecognizer.setRecognitionListener(object : RecognitionListener {
+            override fun onResults(results: Bundle?) {
+                val speechText = results?.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION)?.firstOrNull()
+                speechText?.let {
+                    onResultCallback?.invoke(it)
+                }
+            }
+            override fun onReadyForSpeech(p0: Bundle?) {}
+
+            override fun onBeginningOfSpeech() {}
+
+            override fun onRmsChanged(p0: Float) {}
+
+            override fun onBufferReceived(p0: ByteArray?) {}
+
+            override fun onEndOfSpeech() {}
+
+            override fun onError(p0: Int) {}
+
+
+            override fun onPartialResults(p0: Bundle?) {}
+
+            override fun onEvent(p0: Int, p1: Bundle?) {}
+
+        })
+        speechRecognizer.startListening(recognizerIntent)
+    }
+
+    // 음성 인식 멈춤
+    fun stopListening() {
+        speechRecognizer.stopListening()
+    }
+
+    // 음성 인식 취소
+    fun cancelListening() {
+        speechRecognizer.cancel()
+    }
+}
