6454174e3b80a58c0fece9b5f3f09c7c4826a2a0
JaeYoun Kim <rlawodusx@naver.com>
2025-03-02 03:25:44 +0900

    feat: Add MediaPlayer Object

---
+++ app/src/main/java/com/timor/kidsstory/presentation/setting/SettingScreen.kt
@@ -0,0 +1,19 @@
+package com.timor.kidsstory.presentation.setting
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.timor.kidsstory.ui.theme.KidsStoryTheme
+
+@Composable
+fun SettingScreen(modifier: Modifier = Modifier) {
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun SettingScreenPreview() {
+    KidsStoryTheme {
+        SettingScreen()
+    }
+}
