e8b92bf77e459f5433e96f6b5a843958e17edf21
choe-yujin <dev.yujinchoe@gmail.com>
2025-03-02 21:33:02 +0900

    fix: exportSchema false

--- app/src/main/java/com/timor/kidsstory/data/local/database/AppDatabase.kt
+++ app/src/main/java/com/timor/kidsstory/data/local/database/AppDatabase.kt
@@ -10,7 +10,7 @@ import com.timor.kidsstory.data.local.database.dao.DownloadedBooksDao
 import com.timor.kidsstory.data.local.database.entity.AvailableBookEntity
 import com.timor.kidsstory.data.local.database.entity.DownloadedBookEntity
 
-@Database(entities = [DownloadedBookEntity::class, AvailableBookEntity::class], version = 1)
+@Database(entities = [DownloadedBookEntity::class, AvailableBookEntity::class], version = 1, exportSchema = false)
 @TypeConverters(Converters::class)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun downloadedBooksDao(): DownloadedBooksDao
