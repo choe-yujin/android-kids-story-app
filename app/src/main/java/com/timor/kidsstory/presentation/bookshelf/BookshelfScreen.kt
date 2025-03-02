eedf35d787fd94ada287b109ccb4ef2c4be5bfff
JaeYoun Kim <rlawodusx@naver.com>
2025-03-02 19:52:43 +0900

    feat: Add Setting Screen

--- app/src/main/java/com/timor/kidsstory/presentation/bookshelf/BookshelfScreen.kt
+++ app/src/main/java/com/timor/kidsstory/presentation/bookshelf/BookshelfScreen.kt
@@ -5,28 +5,21 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.safeContentPadding
-import androidx.compose.foundation.layout.systemBarsPadding
-import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
-import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.timor.kidsstory.presentation.bookshelf.components.BookCover
 import com.timor.kidsstory.presentation.bookshelf.components.BookshelfHeader
 import com.timor.kidsstory.presentation.bookshelf.model.BookshelfUiState
+import com.timor.kidsstory.ui.theme.KidsStoryTheme
 
 // 책장 화면 UI 컴포넌트
 @Composable
@@ -72,5 +65,20 @@ fun BookshelfScreen(
                 }
             }
         }
+    }
+}
+
+@Preview(showBackground = true, widthDp = 600, heightDp = 300)
+@Composable
+private fun BookShelfScreenPreview() {
+    KidsStoryTheme {
+        BookshelfScreen(
+            state = BookshelfUiState(),
+            onChatbotClick = {},
+            onLanguageClick = {},
+            onMakerClick = {},
+            onBookSelected = {}
+        )
+
     }
 }
