30116afb30161498312631eebfc1540468f27f9e
choe-yujin <dev.yujinchoe@gmail.com>
2025-03-03 00:23:11 +0900

    style: Add color palette system

--- app/src/main/java/com/timor/kidsstory/ui/theme/Color.kt
+++ app/src/main/java/com/timor/kidsstory/ui/theme/Color.kt
@@ -2,10 +2,59 @@ package com.timor.kidsstory.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+object AppColors {
+    // Primary colors
+    val primary50 = Color(0xFFFFF9E0)
+    val primary100 = Color(0xFFFFF3C1)
+    val primary200 = Color(0xFFFFE892)
+    val primary300 = Color(0xFFFFDD7C)
+    val primary400 = Color(0xFFFDDE5A)
+    val primary500 = Color(0xFFE3C44A)
+    val primary600 = Color(0xFFC9A93B)
+    val primary700 = Color(0xFFAF8F2D)
+    val primary900 = Color(0xFF7C5C12)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+    // Secondary colors
+    val secondary200 = Color(0xFFC5FF7A)
+    val secondary600 = Color(0xFF47A714)
+
+    // Neutral colors
+    val neutralWhite = Color(0xFFFFFFFF)
+    val neutral50 = Color(0xFFFAFAFA)
+    val neutral100 = Color(0xFFF5F5F5)
+    val neutral200 = Color(0xFFE5E5E5)
+    val neutral300 = Color(0xFFD4D4D4)
+    val neutral400 = Color(0xFFA3A3A3)
+    val neutral500 = Color(0xFF737373)
+    val neutral600 = Color(0xFF525252)
+    val neutral700 = Color(0xFF404040)
+    val neutral800 = Color(0xFF262626)
+    val neutral900 = Color(0xFF171717)
+    val neutral950 = Color(0xFF0A0A0A)
+    val neutralBlack = Color(0xFF000000)
+
+    // Red colors
+    val red600 = Color(0xFFE32F27)
+    val red700 = Color(0xFFC52820)
+
+    // YellowRed colors
+    val yellowRed500 = Color(0xFFFF9500)
+    val yellowRed600 = Color(0xFFE38400)
+    val yellowRed700 = Color(0xFFC57600)
+
+    // Green colors
+    val green50 = Color(0xFFE9FCE9)
+    val green100 = Color(0xFFC3F6C4)
+    val green600 = Color(0xFF2DA148)
+    val green700 = Color(0xFF237D38)
+
+    // Blue colors
+    val blue50 = Color(0xFFE4F2FF)
+    val blue100 = Color(0xFFB8DBFF)
+    val blue400 = Color(0xFF2E93FF)
+    val blue500 = Color(0xFF5A98FD)
+    val blue700 = Color(0xFF1E5FAD)
+
+    // Purple colors
+    val purple700 = Color(0xFF67387A)
+}
