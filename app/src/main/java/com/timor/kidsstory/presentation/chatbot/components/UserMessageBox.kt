9f01ffcba6f40c59d541c6d3b8f49d2272989e76
JaeYoun Kim <rlawodusx@naver.com>
2025-03-03 21:45:14 +0900

    Fix: Chat Design Fix

--- app/src/main/java/com/timor/kidsstory/presentation/chatbot/components/UserMessageBox.kt
+++ app/src/main/java/com/timor/kidsstory/presentation/chatbot/components/UserMessageBox.kt
@@ -2,19 +2,20 @@ package com.timor.kidsstory.presentation.chatbot.components
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -36,7 +37,7 @@ fun UserMessageBox(
         Text(
             modifier = Modifier
                 .background(
-                    color = AppColors.neutralWhite,
+                    color = AppColors.blue600,
                     shape = RoundedCornerShape(12.dp)
                 )
                 .padding(vertical = 8.dp, horizontal = 36.dp),
@@ -45,11 +46,18 @@ fun UserMessageBox(
 
         Spacer(modifier = Modifier.width(8.dp))
 
-        Icon(
-            modifier = Modifier.size(48.dp),
-            painter = painterResource(R.drawable.chat_user), contentDescription = null,
-            tint = Color.Unspecified,
-        )
+        Box(
+            modifier = Modifier
+                .size(48.dp)
+                .background(color = AppColors.blue600, shape = CircleShape)
+                .padding(8.dp)
+        ) {
+            Icon(
+                painter = painterResource(R.drawable.ic_user),
+                contentDescription = "user",
+                tint = AppColors.neutralWhite,
+            )
+        }
     }
 }
 
