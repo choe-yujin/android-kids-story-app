d5e09754139057c8eeba3a7b57058641db078e8e
choe-yujin <dev.yujinchoe@gmail.com>
2025-03-30 21:15:03 +0900

    feat: bookcoverUiState에 다운로드 상태, 원격저장소 책 id 추가

--- app/src/main/java/com/timor/kidsstory/presentation/bookshelf/model/BookCoverUiState.kt
+++ app/src/main/java/com/timor/kidsstory/presentation/bookshelf/model/BookCoverUiState.kt
@@ -1,5 +1,7 @@
 package com.timor.kidsstory.presentation.bookshelf.model
 
+import com.timor.kidsstory.domain.model.DownloadStatus
+
 /**
  * 책 표지 UI 상태 클래스
  * - 책장 화면에서 표시되는 개별 책 표지의 상태 정보
@@ -7,9 +9,13 @@ package com.timor.kidsstory.presentation.bookshelf.model
  * @property imageUrl 책 표지 이미지 URL
  * @property title 책 제목
  * @property storyId 책 고유 ID (예: 801_en-ph)
+ * @property downloadStatus 다운로드 상태
+ * @property remoteId 원격 저장소의 책 ID
  */
 data class BookCoverUiState(
     val imageUrl: String,
     val title: String,
-    val storyId: String
+    val storyId: String,
+    val downloadStatus: DownloadStatus = DownloadStatus.DOWNLOADED,
+    val remoteId: Int = 0
 )
