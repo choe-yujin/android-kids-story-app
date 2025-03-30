94d57b5233ba3090de8490493be97c240376bae5
choe-yujin <dev.yujinchoe@gmail.com>
2025-03-31 02:17:44 +0900

    fix: text/plain으로 받은 응답을 직접 문자열로 변환

--- app/src/main/java/com/timor/kidsstory/data/remote/network/BookNetworkService.kt
+++ app/src/main/java/com/timor/kidsstory/data/remote/network/BookNetworkService.kt
@@ -5,10 +5,12 @@ import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.request.get
 import io.ktor.client.statement.bodyAsChannel
+import io.ktor.client.statement.bodyAsText
 import io.ktor.http.isSuccess
 import io.ktor.utils.io.jvm.javaio.copyTo
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import kotlinx.serialization.json.Json
 import java.io.File
 import javax.inject.Inject
 
@@ -34,7 +36,13 @@ class BookNetworkService @Inject constructor(
      * @return 메타데이터 객체
      */
     suspend fun getMetadata(): GithubMetadata {
-        return httpClient.get("https://raw.githubusercontent.com/choe-yujin/storybook-assets/master/metadata.json").body()
+        val response = httpClient.get("https://raw.githubusercontent.com/choe-yujin/storybook-assets/master/metadata.json")
+
+        // text/plain으로 받은 응답을 직접 문자열로 변환하고
+        val jsonString = response.bodyAsText()
+
+        // 그 다음 문자열을 JSON으로 파싱
+        return Json.decodeFromString<GithubMetadata>(jsonString)
     }
 
     /**
