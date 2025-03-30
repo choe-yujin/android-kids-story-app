5bc16efa6b5413d3fb6507de34a1784bcf12808f
choe-yujin <dev.yujinchoe@gmail.com>
2025-03-30 21:21:59 +0900

    feat: 의존성 주입 추가

--- app/src/main/java/com/timor/kidsstory/di/RepositoryModule.kt
+++ app/src/main/java/com/timor/kidsstory/di/RepositoryModule.kt
@@ -1,12 +1,18 @@
 package com.timor.kidsstory.di
 
+import android.content.Context
+import com.timor.kidsstory.data.local.database.dao.DownloadedBooksDao
+import com.timor.kidsstory.data.remote.BookDownloader
+import com.timor.kidsstory.data.remote.network.BookNetworkService
 import com.timor.kidsstory.data.repository.BookRepositoryImpl
 import com.timor.kidsstory.data.repository.UserPreferenceRepositoryImpl
 import com.timor.kidsstory.domain.repository.BookRepository
 import com.timor.kidsstory.domain.repository.UserPreferenceRepository
 import dagger.Binds
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
@@ -25,4 +31,14 @@ abstract class RepositoryModule {
     abstract fun bindUserPreferenceRepository(
         userPreferenceRepositoryImpl: UserPreferenceRepositoryImpl
     ): UserPreferenceRepository
+
+    @Provides
+    @Singleton
+    fun provideBookDownloader(
+        @ApplicationContext context: Context,
+        networkService: BookNetworkService,
+        downloadedBooksDao: DownloadedBooksDao
+    ): BookDownloader {
+        return BookDownloader(context, networkService, downloadedBooksDao)
+    }
 }
