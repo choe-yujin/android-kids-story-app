30258251f9c80202b5c0aefe80ec3fab9b1d1340
choe-yujin <dev.yujinchoe@gmail.com>
2025-04-18 18:22:52 +0900

    refactor(model): 다운로드 상태 모델 개선
       - 다운로드 상태 관리 로직 명확화
       - 상태 변경 추적 용이성 개선

--- app/src/main/java/com/timor/kidsstory/domain/model/DownloadStatus.kt
+++ app/src/main/java/com/timor/kidsstory/domain/model/DownloadStatus.kt
@@ -4,8 +4,8 @@ package com.timor.kidsstory.domain.model
  * 책 다운로드 상태
  */
 enum class DownloadStatus {
-    AVAILABLE,    // 다운로드 가능
-    DOWNLOADING,  // 다운로드 진행 중
-    DOWNLOADED,   // 다운로드 완료
-    FAILED        // 다운로드 실패
+    AVAILABLE,     // 다운로드 가능
+    DOWNLOADING,   // 다운로드 중
+    DOWNLOADED,    // 이미 다운로드됨
+    FAILED         // 다운로드 실패
 }
