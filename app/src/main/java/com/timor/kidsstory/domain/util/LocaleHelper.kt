2a1c369a2838ccfc78de6f3c63372faff513fd0c
JaeYoun Kim <rlawodusx@naver.com>
2025-04-28 16:31:02 +0900

    Fix: 다국어 설정 관련 오류사항 수정
    - 현재 저장된 국가 설정이 없을 경우 시스템 언어 설정을 따라가도록 수정
    - 설정된 국가 설정이 있을 경우 해당 국가 설정을 따라가도록 변경

--- app/src/main/java/com/timor/kidsstory/domain/util/LocaleHelper.kt
+++ app/src/main/java/com/timor/kidsstory/domain/util/LocaleHelper.kt
@@ -1,56 +1,70 @@
 package com.timor.kidsstory.domain.util
 
+import android.app.LocaleManager
 import android.content.Context
-import com.timor.kidsstory.domain.util.LanguageConstants.getResourceLangCode
+import android.os.Build
+import android.os.LocaleList
+import com.orhanobut.logger.Logger
 import java.util.Locale
 
 /*
 * 앱내에서 로케일 설정을 관리하는 헬퍼 클래스
 * */
 object LocaleHelper {
-
-//    /*
-//    * 제공된 언어코드에 따라 컨텍스트의 로케일 변경
-//    *
-//    * */
-//
-//    fun setLocale(context: Context, languageCode: String): Context {
-//        // 언어 코드 정규화
-//        val resourceCode = LanguageConstants.getResourceLangCode(languageCode)
-//        Logger.e("언어 변경전 확인: $resourceCode")
-//        return updateResources(context, resourceCode)
-//    }
-//
-//
-//    /*
-//    * 리소스 설정을 업데이트 하여 새로운 로케일이 적용된 컨텍스트 반환
-//    * */
-//    private fun updateResources(context: Context, languageCode: String): Context {
-//        val locale = Locale(languageCode)
+//    fun Context.updateLanguage(languageCode: String) {
+//        val newLanguageCode = getResourceLangCode(languageCode)
+//        val locale = Locale(newLanguageCode)
 //        Locale.setDefault(locale)
 //
-//        val configuration = Configuration(context.resources.configuration)
+//        val config = resources.configuration
+//        config.setLocale(locale)
+//        config.setLayoutDirection(locale)
 //
-//        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.N){
-//            val localeList = LocaleList(locale)
-//            LocaleList.setDefault(localeList)
-//            configuration.setLocales(localeList)
-//        } else {
-//            configuration.locale = locale
-//        }
-//
-//        return context.createConfigurationContext(configuration)
+//        resources.updateConfiguration(config, resources.displayMetrics)
 //    }
 
+
     fun Context.updateLanguage(languageCode: String) {
-        val newLanguageCode = getResourceLangCode(languageCode)
-        val locale = Locale(newLanguageCode)
-        Locale.setDefault(locale)
+        try {
+            Logger.e("언어 코드 확인1: $languageCode")
+            val newLanguageCode = when {
+                languageCode.contains("en") -> "en"
+                languageCode.contains("ko") -> "ko"
+                languageCode.contains("tet") -> "tet"
+                else -> "en"
+            }
 
-        val config = resources.configuration
-        config.setLocale(locale)
-        config.setLayoutDirection(locale)
+            Logger.e("언어 코드 확인2: $newLanguageCode")
 
-        resources.updateConfiguration(config, resources.displayMetrics)
+
+            val locale = Locale(newLanguageCode)
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                val localeList = LocaleList(locale)
+                (getSystemService(Context.LOCALE_SERVICE) as LocaleManager).applicationLocales = localeList
+            } else {
+                Locale.setDefault(locale)
+                val resources = applicationContext.resources
+                val configuration = resources.configuration
+                configuration.setLocale(locale)
+                resources.updateConfiguration(configuration, resources.displayMetrics)
+            }
+        } catch (e: Exception) {
+            Logger.e("MainActivity", "updateLanguage: Error updating language $e")
+        }
+    }
+
+    // 시스템 언어를 감지하고 앱에서 지원하는 언어 코드로 변환
+    fun getSystemLanguageCode(context: Context): String {
+        // API 24 기준으로 시스템 언어를 가져오는 방법 분기 처리
+        val systemLocale = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            context.resources.configuration.locales[0]
+        } else {
+            @Suppress("DEPRECATION")
+            context.resources.configuration.locale
+        }
+
+        val languageCode = systemLocale.language
+        return languageCode
     }
 }
