f1e80ce05c7b01336d6644e3425efddd55c80886
JaeYoun Kim <rlawodusx@naver.com>
2025-04-28 16:31:02 +0900

    Fix: 다국어 설정 관련 오류사항 수정
    - 현재 저장된 국가 설정이 없을 경우 시스템 언어 설정을 따라가도록 수정
    - 설정된 국가 설정이 있을 경우 해당 국가 설정을 따라가도록 변경

--- app/src/main/java/com/timor/kidsstory/domain/util/LanguageManager.kt
+++ app/src/main/java/com/timor/kidsstory/domain/util/LanguageManager.kt
@@ -1,10 +1,5 @@
 package com.timor.kidsstory.domain.util
 
-import com.orhanobut.logger.Logger
-import kotlinx.coroutines.flow.MutableSharedFlow
-import kotlinx.coroutines.flow.SharedFlow
-import kotlinx.coroutines.flow.asSharedFlow
-
 
 /*
 * 언어 변겨 이벤트 및 현재 언어 코드를 관리하는 싱글톤
@@ -13,22 +8,11 @@ object LanguageManager {
     // 현재 설정된 언어코드
     private var currentLanguageCode: String = LanguageConstants.getResourceLangCode(LanguageConstants.DEFAULT_LANGUAGE.code)
 
-    // 언어 변경 이벤트를
-    private val _languageChangeFlow = MutableSharedFlow<String>(replay = 1)
-    val languageChangeFlow: SharedFlow<String> = _languageChangeFlow.asSharedFlow()
-
     // 현재 설정된 언어코드 반환
     fun getCurrentLanguageCode(): String = currentLanguageCode
 
-    /**
-     * 언어 코드 업데이트 및 변경 이벤트 발행
-     *
-     */
-    suspend fun updateLanguageCode(newLanguageCode: String) {
-        // 비어있는 경우 기본값 사용
-        val codeToUse = newLanguageCode.ifEmpty { LanguageConstants.DEFAULT_LANGUAGE.code }
-        val newString = LanguageConstants.getResourceLangCode(codeToUse)
-        Logger.e("언어 newString: $newString")
-        _languageChangeFlow.emit(newString)
+    // 언어 코드 설정 함수 추가
+    fun setCurrentLanguageCode(code: String) {
+        currentLanguageCode = LanguageConstants.getResourceLangCode(code)
     }
 }
